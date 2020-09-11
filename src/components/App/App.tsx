da387bc43cec267fe74086be94ce36c0248e21e4
Gaya Kessler <gaya@theclevernode.com>
2020-09-11 14:48:08 +0200

    introduce Material UI

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import Routes from '../../Routes';
 
 const App: React.FC = () => {
-  const test = '';
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () => createMuiTheme({
+      palette: {
+        type: prefersDarkMode ? 'dark' : 'light',
+      },
+      typography: {
+        fontFamily: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ].join(','),
+      },
+    }),
+    [prefersDarkMode],
+  );
 
   return (
-    <div className="App">
-      <h1>Improved Spotify</h1>
-      <Routes />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <h1>Improved Spotify</h1>
+        <Routes />
+      </div>
+    </ThemeProvider>
   );
 };
 
