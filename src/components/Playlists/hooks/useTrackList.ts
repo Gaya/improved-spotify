c3406c5bb45a492c57f4dfb834d456bee56b9bc5
Gaya Kessler <gaya@theclevernode.com>
2020-10-02 15:04:12 +0200

    refactor to use reducer instead of useState

--- src/components/Playlists/hooks/useTrackList.ts
+++ src/components/Playlists/hooks/useTrackList.ts
@@ -1,9 +1,14 @@
 import {
-  useCallback, useContext, useEffect, useRef, useState,
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
 } from 'react';
 import { useRecoilState } from 'recoil';
 
 import {
+  PagedResponse,
   SpotifyDataExport, SpotifyPlaylistTrack, StoredSpotifyPlaylistTrack, TrackState,
 } from '../../../types';
 import { SPOTIFY_PLAYLIST_TRACKS } from '../../../consts';
@@ -18,6 +23,90 @@ import {
 } from '../../../database/queries';
 import { playlistTracksState } from '../../../state/atoms';
 
+interface UseTrackListState {
+  isResolving: boolean;
+  isResolved: boolean;
+  needsFetching: boolean;
+  showProgress: boolean;
+  totalTracks: number;
+  fetchedTracks: SpotifyPlaylistTrack[];
+  tracks: StoredSpotifyPlaylistTrack[];
+}
+
+const defaultState: UseTrackListState = {
+  isResolving: false,
+  isResolved: false,
+  needsFetching: false,
+  showProgress: false,
+  totalTracks: 0,
+  fetchedTracks: [],
+  tracks: [],
+};
+
+interface UpdateTrackData {
+  type: 'UPDATE_TRACK_DATA';
+  payload: StoredSpotifyPlaylistTrack[];
+}
+
+interface StartFetching {
+  type: 'START_FETCHING';
+}
+
+interface ContinueFetching {
+  type: 'CONTINUE_FETCHING';
+}
+
+interface StartResolving {
+  type: 'START_RESOLVING';
+}
+
+interface ReceiveTracks {
+  type: 'RECEIVE_TRACKS';
+  payload: PagedResponse<SpotifyPlaylistTrack>;
+}
+
+type UseTrackListAction = UpdateTrackData | StartFetching | StartResolving | ReceiveTracks
+  | ContinueFetching;
+
+function reducer(state: UseTrackListState, action: UseTrackListAction): UseTrackListState {
+  switch (action.type) {
+    case 'RECEIVE_TRACKS':
+      return {
+        ...state,
+        totalTracks: action.payload.total,
+        fetchedTracks: [...state.fetchedTracks, ...action.payload.items],
+        needsFetching: !!action.payload.next,
+      };
+    case 'START_RESOLVING':
+      return {
+        ...state,
+        isResolving: true,
+      };
+    case 'START_FETCHING':
+      return {
+        ...state,
+        needsFetching: true,
+        showProgress: true,
+      };
+    case 'CONTINUE_FETCHING':
+      return {
+        ...state,
+        needsFetching: false,
+      };
+    case 'UPDATE_TRACK_DATA':
+      return {
+        ...state,
+        tracks: action.payload,
+        isResolved: true,
+        isResolving: false,
+        needsFetching: false,
+        fetchedTracks: [],
+      };
+    default:
+      return state;
+  }
+}
+
 function useTrackList(id: string): {
   progress: number;
   tracks: StoredSpotifyPlaylistTrack[];
@@ -27,25 +116,32 @@ function useTrackList(id: string): {
   const db = useContext(DatabaseContext);
   const [tracksState, setTracksState] = useRecoilState(playlistTracksState);
 
-  const [isResolving, setIsResolving] = useState(false);
-  const [isResolved, setIsResolved] = useState(false);
-  const [isFetching, setIsFetching] = useState(false);
-  const [showProgress, setShowProgress] = useState(false);
-  const [totalTracks, setTotalTracks] = useState(0);
-  const [fetchedTracks, setFetchedTracks] = useState<SpotifyPlaylistTrack[]>([]);
-  const [tracks, setTracks] = useState<StoredSpotifyPlaylistTrack[]>([]);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const {
+    isResolved,
+    isResolving,
+    needsFetching,
+    showProgress,
+    totalTracks,
+    fetchedTracks,
+    tracks,
+  } = state;
+
   const nextRef = useRef(SPOTIFY_PLAYLIST_TRACKS.replace('{id}', id));
 
   // update tracks in state
   const updateTrackData = useCallback((playlistTracks: StoredSpotifyPlaylistTrack[]) => {
-    setTracks(playlistTracks);
-    setIsResolved(true);
+    dispatch({
+      type: 'UPDATE_TRACK_DATA',
+      payload: playlistTracks,
+    });
   }, []);
 
   // store tracks in database
   const storeTrackData = useCallback((data: SpotifyDataExport): void => {
     if (db) {
-      info('Update cached data');
+      info('Save cached data to database');
       storeDataExport(db, data)
         .then(() => {
           setTracksState({
@@ -59,36 +155,34 @@ function useTrackList(id: string): {
 
   // fetching mechanism
   useEffect(() => {
-    if (isResolving && isFetching && !isResolved) {
-      setIsFetching(false);
+    if (isResolving && needsFetching && !isResolved) {
+      dispatch({ type: 'CONTINUE_FETCHING' });
 
-      // no result... let's fetch them from Spotify
       info(`Fetching ${nextRef.current}`);
 
       getPlaylistTracks(nextRef.current).then((response) => {
-        const allTracks = [...fetchedTracks, ...response.items];
-
-        if (totalTracks !== response.total) {
-          setTotalTracks(response.total);
-        }
-
-        setFetchedTracks(allTracks);
-
         if (response.next) {
           nextRef.current = response.next;
-          setIsFetching(true);
-        } else {
+        }
+
+        dispatch({
+          type: 'RECEIVE_TRACKS',
+          payload: response,
+        });
+
+        if (!response.next) {
+          const allTracks = [...fetchedTracks, ...response.items];
           const extracted = extractTrackData(id, allTracks);
           storeTrackData(extracted);
         }
       });
     }
-  }, [fetchedTracks, id, isFetching, isResolved, isResolving, storeTrackData, totalTracks]);
+  }, [fetchedTracks, id, isResolved, isResolving, needsFetching, storeTrackData]);
 
   // find tracks in cache or start fetching
   useEffect(() => {
-    if (!isFetching && db && tracksState[id] && !isResolving && !isResolved) {
-      setIsResolving(true);
+    if (!needsFetching && db && tracksState[id] && !isResolving && !isResolved) {
+      dispatch({ type: 'START_RESOLVING' });
 
       ((): Promise<void> => {
         if (tracksState[id] === TrackState.INVALID) {
@@ -103,15 +197,15 @@ function useTrackList(id: string): {
         .then((result) => {
           if (result.length > 0 && tracksState[id] === TrackState.VALID) {
             // has result... so resolve it!
+            info('Using cached data from database');
             updateTrackData(result);
           } else {
             // no results? Start fetching process
-            setShowProgress(true);
-            setIsFetching(true);
+            dispatch({ type: 'START_FETCHING' });
           }
         });
     }
-  }, [db, id, isFetching, isResolved, isResolving, tracksState, updateTrackData]);
+  }, [db, id, isResolved, isResolving, needsFetching, tracksState, updateTrackData]);
 
   return {
     tracks,
