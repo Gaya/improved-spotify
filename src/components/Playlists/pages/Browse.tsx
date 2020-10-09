d0a9d1c450d2ed9ef89e801acce2e14e036fc572
Gaya Kessler <gaya@theclevernode.com>
2020-10-09 09:46:17 +0200

    updated dependencies

--- src/components/Playlists/pages/Browse.tsx
+++ src/components/Playlists/pages/Browse.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import Layout from '../../App/Layout';
 import PageTitle from '../../PageTitle/PageTitle';
 import PageContainer from '../../App/PageContainer';
+import Container from '../../Container/Container';
 
 const Browse: React.FC = () => (
   <Layout>
     <PageContainer>
-      <PageTitle>
-        Browse Your Playlists
-      </PageTitle>
+      <Container>
+        <PageTitle>
+          Browse Your Playlists
+        </PageTitle>
+      </Container>
     </PageContainer>
   </Layout>
 );
